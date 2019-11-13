3cdd74ef6481852eaf2c26e9f11154141fc1490b
Hao Zhong <alfred.zhong@centre.edu>
2019-11-13 18:05:43 -0500

    haha

--- lab.js
+++ lab.js
@@ -3,12 +3,14 @@
 var penpromise= d3.json("penguins/classData.json")
 
 penpromise.then(
-function(penguin)
+function(data)
 {
-    console.log("penguin",penguin);
+    var quizdata = quizgrade(data);
+    console.log(quizdata);
     seth1("Graph of the Quizes");
-    setup(penguin);
+    setup(quizdata);
 },
+    
 function(err)
 {
     seth1("No penguin");
@@ -20,20 +22,30 @@ var seth1 = function(message)
 }
 
 
-var getquiz=function(quiz)
+var quizgrade = function(classroom)
+{
+    var quizdata = classroom.map(quizmap)
+    return quizdata
+}
+
+var quizmap = function(penguin)
+{
+    var allquiz = penguin.quizes.map(getGrade)
+    penguin.qgrade=allquiz
+    return penguin
+}
+
+
+var getGrade = function(quiz)
 {
     return quiz.grade
 }
 
-var getQuiz=function(penguin)
- {
-     return penguin.quizes.map(getquiz)
- }
 
 var screen = {width:800,height:500}
 var margins={top:10,right:50,bottom:50,left:50}
 
-var setup=function(penguin)
+var setup=function(classroom)
 {
     d3.select("svg")
     .attr("width",screen.width)
@@ -74,35 +86,27 @@ var setup=function(penguin)
     .attr("transform","translate(25,"+margins.top+")")
     .call(yAxis)
     
-
-    drawpath(penguin,xScale,yScale,cScale)
+    drawpath(classroom,xScale,yScale)
     
 }
 
-var drawpath=function(penguin,xScale,yScale,cScale)
+var drawpath=function(classroom,xScale,yScale)
 {
-    
-    var quizes = penguin.map(function(penguin){
-        return penguin.quizes;
-    })
-    var quizGrades = quizes.map(function(quiz){
-        return quiz.grade;
-    })
     
     
     var arrays=d3.select("#graph")
     .selectAll("g")
-    .data(penguin)
+    .data(classroom)
     .enter()
     .append("g")
+    .attr("id","line")
     .attr("fill","none")
     .attr("stroke","black")
-    .attr("stroke-width", 3)
-    .attr("stroke",function(quizes)
-       {
-        return cScale(quizes.name);
-    })
-    .attr("stroke-width",3)
+    .attr("stroke-width", 2.5)
+    .on("mouseover",function(d,index)
+    {
+                 info(classroom,index)
+                 })
     
     var lineGenerator=d3.line()
     .x(function(num,index){return xScale(index)})
@@ -111,7 +115,17 @@ var drawpath=function(penguin,xScale,yScale,cScale)
     
     arrays
     .append('path')
-    .datum(function(obj){return obj.quizes})
+    .datum(function(obj){return obj.qgrade})
     .attr("d",lineGenerator)
-    
 }
+
+var info=function(classroom)
+{
+    d3.select("div")
+    .selectAll("img")
+    .data(classroom)
+    .enter()
+    .append("img")
+    .attr("src",function(d,i){return d.picture})
+}
+
