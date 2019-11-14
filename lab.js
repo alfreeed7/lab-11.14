f9d300a8daceaa4d70222c3e1987a29177db41b6
Hao Zhong <alfred.zhong@centre.edu>
2019-11-13 20:42:09 -0500

    ok

--- lab.js
+++ lab.js
@@ -3,12 +3,11 @@
 var penpromise= d3.json("penguins/classData.json")
 
 penpromise.then(
-function(data)
+function(penguin)
 {
-    var quizdata = quizgrade(data);
-    console.log(quizdata);
+   
     seth1("Graph of the Quizes");
-    setup(quizdata);
+    setup(penguin);
 },
     
 function(err)
@@ -22,30 +21,11 @@ var seth1 = function(message)
 }
 
 
-var quizgrade = function(classroom)
-{
-    var quizdata = classroom.map(quizmap)
-    return quizdata
-}
 
-var quizmap = function(penguin)
-{
-    var allquiz = penguin.quizes.map(getGrade)
-    penguin.qgrade=allquiz
-    return penguin
-}
-
-
-var getGrade = function(quiz)
-{
-    return quiz.grade
-}
-
-
-var screen = {width:800,height:500}
+var screen = {width:750,height:500}
 var margins={top:10,right:50,bottom:50,left:50}
 
-var setup=function(classroom)
+var setup=function(penguin)
 {
     d3.select("svg")
     .attr("width",screen.width)
@@ -86,46 +66,48 @@ var setup=function(classroom)
     .attr("transform","translate(25,"+margins.top+")")
     .call(yAxis)
     
-    drawpath(classroom,xScale,yScale)
+    drawpath(penguin,xScale,yScale)
     
 }
 
-var drawpath=function(classroom,xScale,yScale)
+var drawpath=function(penguin,xScale,yScale)
 {
     
     
     var arrays=d3.select("#graph")
     .selectAll("g")
-    .data(classroom)
+    .data(penguin)
     .enter()
     .append("g")
     .attr("id","line")
     .attr("fill","none")
     .attr("stroke","black")
-    .attr("stroke-width", 2.5)
-    .on("mouseover",function(d,index)
-    {
-                 info(classroom,index)
-                 })
+    .attr("stroke-width", 2)
+   
     
     var lineGenerator=d3.line()
     .x(function(num,index){return xScale(index)})
     .y(function(num){return yScale(num)})
     .curve(d3.curveNatural)
     
+   d3.select("body")
+        .append("div")
+        .attr("class", "penimg");
+    
     arrays
     .append('path')
-    .datum(function(obj){return obj.qgrade})
+    .datum(function(obj){return obj.quizes.map(function(d){return d.grade;})})
     .attr("d",lineGenerator)
-}
-
-var info=function(classroom)
-{
-    d3.select("div")
-    .selectAll("img")
-    .data(classroom)
-    .enter()
-    .append("img")
-    .attr("src",function(d,i){return d.picture})
+.on('mouseover', function(d, i)
+           {
+                d3.select(".penimg *").remove();
+                d3.select(".penimg")
+                    .append("img")
+                    .attr("src", "penguins/" + penguin[i].picture);
+            })
+        .on('mouseout', function()
+            {
+                d3.select(".penimg *").remove();
+            })
 }
 
